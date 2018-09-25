33acd53e43a3d0ea3c7aa515f21fd4b782dd0f2f
Yana Bashtyk <Yana_bashtyk@epam.com>
2018-09-25 03:47:14 +0300

    soultion advanced zeros.attempt

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  
+  let power, 
+      zerosCount = number;
+
+  //divide base-number in prime(simple) factors
+  for (let i=2; i<=base; i++) {
+    
+    if (base % i == 0){
+      power = 0;
+    //find power for each factor (i)  
+      while (base % i == 0){
+        power++;
+        base /= i;
+      }
+      let counter=0;
+      let numberTemp = number;
+      //count the number of factors in factorial
+      while (numberTemp / i > 0){
+        counter += Math.floor(numberTemp/i);
+        numberTemp = Math.floor(numberTemp/i);
+      }
+      
+      if (zerosCount > Math.floor(counter/power)) zerosCount = Math.floor(counter/power);
+    }
+  }
+  return zerosCount;
 }
